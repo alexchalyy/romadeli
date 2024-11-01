eefac6de2333eb95dca31cd35ce197e601341cbd
alexchalyy <alexchalyy@gmail.com>
2024-11-01 13:16:08 -0400

    scroll bar inserted into Menu page

--- DOTNETPROJECT2/obj/Debug/net8.0/DOTNETPROJECT2.AssemblyInfo.cs
+++ DOTNETPROJECT2/obj/Debug/net8.0/DOTNETPROJECT2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DOTNETPROJECT2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d9254a6e026e056338d207823a9977330d590a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da2f2d2aa9e82a519b65223018658289e4a370c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DOTNETPROJECT2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DOTNETPROJECT2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
